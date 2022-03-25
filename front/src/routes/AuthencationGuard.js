0c251bf4da6345f6c63ad7dad760a7d9d0b5167f
Callan <cerveauxallanjean@gmail.com>
2022-03-25 17:03:03 +0100

    Make route and AuthenticationGuard

---
+++ front/src/routes/AuthencationGuard.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { authenticationCheck, logout, selectAuth } from '../app/authSlice';
+import { PENDING } from '../config/constant';
+
+export default function AuthenticationGuard() {
+  const { status, isAuthenticated, error } = useSelector(selectAuth)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    dispatch(authenticationCheck())
+  }, [dispatch])
+
+  return status === PENDING ?
+    (
+      <p>loading...</p>
+    ) : (
+      <>
+        { error }
+        <RoutedComponent />
+      </>
+    )
+
+
+  function RoutedComponent() {
+    const location = useLocation()
+    const isLoginPagePathname = location.pathname.includes('login')
+    if (isAuthenticated) {
+      if (isLoginPagePathname) {
+        const { from = { pathname: '/profile' } } = location;
+        return <Navigate to={from.pathname} state={{ from }} replace />;
+      }
+      return (
+        <>
+          <Outlet />
+          <button
+            onClick={async () => {
+              await dispatch(logout());
+              navigate('/');
+            }}
+          >
+            logout
+          </button>
+        </>
+      ) 
+    } else {
+      if (isLoginPagePathname) {
+        return <Outlet />;
+      }
+
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+  }
+}
--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -1,10 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
+import Errror from '../components/Error';
+import Login from '../components/Login';
+import Dashboard from '../containers/Dashboard';
+import Home from '../containers/Home';
+import AuthenticationGuard from './AuthencationGuard';
 
 export default function MainRoutes() {
   return (
     <Routes>
-      <Route path='/login' component='' />
-      <Route path='*' component='' />
+      <Route path='/' element={<Home />} />
+      <Route path='*' element={<Errror />} />
+      <Route element={<AuthenticationGuard/>}>
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Dashboard />} />
+      </Route>
     </Routes>
   )
 }
