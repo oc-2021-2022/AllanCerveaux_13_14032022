ca2eed27bd8eb59221812a6b9e33094e46ef07b7
Callan <cerveauxallanjean@gmail.com>
2022-03-25 17:01:50 +0100

    create constante needed for app

---
+++ front/src/config/constant.js
@@ -0,0 +1,5 @@
+export const API_SERVER = "http://localhost:3001/api/v1";
+export const PENDING = 'PENDING'
+export const SUCCESSFUL = 'SUCCESSFUL'
+export const ERROR = 'ERROR'
+export const TOKEN_KEY = 'token'
